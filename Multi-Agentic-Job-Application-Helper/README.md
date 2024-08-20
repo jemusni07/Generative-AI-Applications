19a6ab24b5f336671112be9589ebb68890983096
Jonathan Musni <jem@Jonathans-MacBook-Air.local>
2024-08-20 17:48:28 -0400

    another set of changes

--- Multi-Agentic-Job-Application-Helper/README.md
+++ Multi-Agentic-Job-Application-Helper/README.md
@@ -2,32 +2,29 @@
 
 This project is inspired when I went through a Deep Learning Course for CrewAI, here is the link - https://www.deeplearning.ai/short-courses/multi-ai-agent-systems-with-crewai/
 
-Generative AI definitely has change the landscape of job applications. Using chatgpt, you can easily prompt your way to create a tailored resume based on a job posting. It became a nightmare to a recruiter on how to distinguish between thousands of ai generated resume. And when they pick one potential good resume, it is embellished to a point that the first phone call interview ends up being a waste of time for both the applicant and the recruiter. 
-
+Generative AI definitely has changed the landscape of job applications. Using chatgpt, you can easily prompt your way to create a tailored resume based on a job posting. It has become a nightmare to a recruiter on how to distinguish between thousands of aAIi generated resume. And whenever they pick one potential good resume, it is embellished to a point that the first phone call interview ends up being a waste of time for both the applicant and the recruiter. It is beneficial to the application process to have a resume that is maximally aligned to the job posting but without the applicant lying about his credentials.
 
 These are some of the pain points when applying for a job posting:
-1. The applicant's work profile does not exactly match job role and descriptions.
-2. Not all of the applicant's experiences are relevant to what the role is looking for.
-3. The applicant is not sure what experiences he/she has that align with the role
-4. Manualy aligning your resume to the role may be a good practice but puts you at a disadvantage with regards to time
 
+1. Not all of the applicant's experiences are relevant to what the role is looking for.
+2. The applicant is not sure what experiences he/she has that align with the role
+3. Manually aligning the resume to the role may be a good practice but puts the applicant at a disadvantage with regards to time especially when it has be done on many applications
 
-The application aims to help the applicant be confident with the resume they are sending to each application.  It will try to align the applicant's profile to the job posting without embellishment and insertion of new skills, fields, industries or experiences just to fit the job posting. It has a guardrail to make sure the generated tailored resume is honest with regards to the applicants profile. The aligned resume should help the applicant to be in good shape for rounds of interviews to follow. This will happen when the applicant is confident that he/she has an honest resume tailored for the job posting.
+The application aims to help the applicant be confident with the resume they are sending to each application.  It will try to align the applicant's profile to the job posting without embellishment and insertion of new skills, fields, industries or experiences just to fit the job posting. It has a guardrail to make sure the generated tailored resume is honest with regards to the applicant’s profile. The aligned resume should help the applicant to be in good shape for rounds of interviews to follow. This will happen when the applicant is confident that he/she has an honest resume tailored for the job posting.
 
 ![image info](./img/honesty_check.png)
 *One important feature of this application is a guardrail against embelishment to make sure the generated resume is honest and with integrity.*
 
 
-This application utilize an hierarchal agentic approach using CrewAI and GPT-4 where each agent has a task and each task is dependent on prior task/s. It uses context RAG instead of vector RAG and that means it relies on LLM to analyze contexts such as the applicants profile and job posting. It is also encouraged to play with agent parameters and prompts to guide the behavior of the whole pipeline. 
+This application utilize a hierarchal agentic approach using CrewAI and GPT-4 where each agent has a task and each task is dependent on prior task/s. It uses context RAG instead of vector RAG and that means it relies on LLM to analyze contexts such as the applicants profile and job posting. It is also encouraged to play with agent parameters and prompts to guide the behavior of the whole pipeline. 
 
 ![image info](./img/multi_agent_job_helper.png)
 *Multi-Agentic Job Application Helper with CrewAI and OpenAI GPT-4*
 
 
-
 What it needs:
-* Resume/Work Profile/CV
-* Job Posting Details
+* Resume/Work Profile/CV - A curriculum vitae is probably a better input to the AI application as it encompasses all of the applicant skills and qualification regardless of its relevancy to the position. The AI app should eventually be the one to pick what skills and qualifications or even make a case on why a non-relevant experience can be helpful to the role the applicant is applying for.
+* Job Posting Details – Put as much detail as possible about the position, the company culture, etc.
 * Cover letter Guide- Guide your cover letter instead of letting the LLM create a generic cover letter. Hiring Managers looks for authenticity.
 
 What it will generate:
@@ -35,5 +32,6 @@ What it will generate:
 * Cover letter that takes into consideration the generated tailored resume, job posting and your cover letter additional inputs
 * Interview preparation material
 
-Eventually, it is on the applicant to take control of its job hunting strategy. This application is just another tool but should not replace being intentional when applying for a certain role or job posting.
-Confidence and integrity is still unmatched and these traits are essential for a healthy career.
+Eventually, it is on the applicant to take control of its job-hunting strategy. This application is just another tool but should not replace being intentional when applying for a certain role or job posting.
+Confidence and integrity are still unmatched and these traits are essential for a healthy career.
+
--- Multi-Agentic-Job-Application-Helper/agent_parameters.py
+++ Multi-Agentic-Job-Application-Helper/agent_parameters.py
@@ -1,170 +1,149 @@
-# AGENTS
+# Research
 
 researcher_agent_goal = """
-    Make sure to do an in-depth analysis 
-    on job_posting to help the job applicant
+    Do an in-depth analysis on job posting to help the job applicant
+"""
+researcher_agent_backstory = """
+    As a Job Researcher, you are strongly capable of navigating and 
+    extracting information from job postings. You can pinpoint the 
+    necessary qualifications and skills sought by employers, forming
+    the foundation for effective application tailoring.
+"""
+research_task_description = """
+    Analyze the job description provided ({job_description})  to extract key skills, 
+    experiences, and qualifications required. Use the tools to gather content and identify 
+    and categorize the requirements.
+"""
+research_task_expected_output = """
+    A structured list of job requirements, including necessary skills, qualifications, 
+    and experiences.
 """
 
-researcher_agent_backstory = """
-     As a Job Researcher, your prowess in 
-    navigating and extracting critical 
-    information from job postings is unmatched.
-    Your skills help pinpoint the necessary 
-    qualifications and skills sought 
-    by employers, forming the foundation for 
-    effective application tailoring.
-"""
+# Job Posting Profiler Agent
 
 profiler_agent_goal = """
-    Do incredible research on job applicants 
-    to help them stand out in the job market
-    Only use the job applicants skills for resume enhancements
+    Do an in-depth research on job applicants experience and qualification
+    to help them stand out in the job market.
 """
-
 profiler_agent_backstory = """
-    Equipped with analytical prowess, you dissect
-    and synthesize information
-    from diverse sources to craft comprehensive 
-    personal and professional profiles, laying the 
-    groundwork for personalized resume enhancements.
+    As a Job Profile analyst, you are strongly capable of dissecting and 
+    synthesizing information from diverse sources to craft comprehensive 
+    personal and professional profiles, laying the groundwork for
+    personalized resume enhancements.
+"""
+profiler_task_description = """
+    Compile a detailed personal and professional profile using personal write-up
+    ({personal_writeup}). Utilize tools to extract and synthesize information from these 
+    sources.
+"""
+profiler_task_expected_output = """
+    A comprehensive profile document that includes skills, project experiences, 
+    contributions, interests, and communication style.
 """
 
+# Resume Strategy Agent
 
 resume_strategist_agent_goal = """
     Make the best and honest resume that aligns with the job posting description.
-    Be as truthful as possible.
+"""
+resume_strategist_agent_backstory = """
+    As a job application strategist, you excel at refining resumes to highlight 
+    the skills and experience relevant to the job posting ensuring that the 
+    resume resonate perfectly with the job's requirements. You are also capable
+    of crafting a resume with honesty and integrity. You do not invent additional
+    skills, experience or field of expertise that the applicant does not currently 
+    have. Instead, you can make a case for the job applicants rigorous experience 
+    and skills that are still relevant to the role.
+"""
+resume_strategy_task_description = """
+    These are the following strategies to follow:
+    * Using the applicant profile and job posting research obtained from previous tasks,
+      tailor the resume to highlight the most relevant areas.
+    * Do not invent new information such as experience, titles, fields and skills that 
+      the applicant does not have. 
+    * Do not exaggerate experiences and skills. 
+    * Include a short overview or summary relevant to the application. 
+    * Update  section, inlcuding the initial summary, work experience, skills, and 
+      education only when necessary to better reflect the candidates abilities and how it matches
+      the job posting.
+    * Emphasize successful metrics achievement.
+    * Maintain credentials that are slightly relevant to the role and job description
+"""
+resume_strategy_task_expected_output = """
+    An tailored resume that effectively highlights the candidate's qualifications 
+    and experiences relevant to the job posting. 
 """
 
-resume_strategist_agent_backstory = """
-    With a strategic mind and an eye for detail, you
-    excel at refining resumes to highlight the most
-    relevant skills and experiences, ensuring they
-    resonate perfectly with the job's requirements.
-    You are also truthful with regards to the skills
-    roles and experiences, you do not need to insert new skills
-    that the engineer does not have
-"""
+# Quality Agent
 
 quality_checker_agent_goal = """
-    Your goal is to make sure that the tailored resume is honest with regards to the original resume,
-    no additional skills or experience added
+    Make the best revision of the tailored resume by investigating and correcting 
+    embellishments
 """
-
 quality_checker_agent_backstory = """
-    As a quality checker, you excel at making sure that the applicant tailored resume is being truthful
-    You will be comparing the generated resume and the original one. You are a good advisor so that the
-    applicant will be in a good position for an interview
+    As Quality Assurance Analyst, you strive to position the applicant in the best
+    shape possible. You have a keen eye for checking for unnecessary embelishments and 
+    and insertion of skills, experience, and field of expertise that the applicant
+    does not really have. You are an honest advisor and want the best for the
+    applicant in the job application process.
 """
-
-cover_letter_generator_goal = """
-    Generate a cover letter based on applicants desired inputs
-"""
-
-cover_letter_generator_backstory = """
-    You generate a cover letter that respects the tone and inputs of the job applicant.
-    You position the applicant in the best and honest way to the hiring manager.
-"""
-
-interview_preparer_agent_goal = """
-    Create interview questions and talking points
-    based on the aligned resume and job description and requirements
-"""
-
-interview_preparer_agent_backstory = """
-    Your role is crucial in anticipating the dynamics of
-    interviews. With your ability to formulate key questions
-    and talking points, you prepare candidates for success,
-    ensuring they can confidently address all aspects of the
-    job they are applying for.
-"""
-
-
-# TASKS
-research_task_description = """
-    Analyze the job description provided ({job_description}) 
-    to extract key skills, experiences, and qualifications 
-    required. Use the tools to gather content and identify 
-    and categorize the requirements.
-"""
-
-research_task_expected_output = """
-    A structured list of job requirements, including necessary
-    skills, qualifications, and experiences.
-"""
-
-profiler_task_description = """
-    Compile a detailed personal and professional profile
-    using personal write-up
-    ({personal_writeup}). Utilize tools to extract and
-    synthesize information from these sources.
-"""
-
-profiler_task_expected_output = """
-    A comprehensive profile document that includes skills, 
-    project experiences, contributions, interests, and 
-    communication style.
-"""
-
-resume_strategy_task_description = """
-    Using the profile and research obtained from
-    previous tasks, tailor the resume to highlight the most
-    relevant areas. 
-    Do not invent new information such as experience, titles,fields
-    and skills that the applicant does not have. 
-    Do not exaggerate experiences and skills.
-    Include a short overview or summary relevant to the application
-    Update  section, inlcuding the initial summary, work experience, skills,
-    and education only when necessary to better reflect the candidates
-    abilities and how it matches the job posting.      
-    Emphasize successful metrics achievement.
-    Retain experiences that is relevant adjacent to the job description
-"""
-
-resume_strategy_task_expected_output = """
-    An updated resume that effectively highlights the candidate's
-    qualifications and experiences relevant to the job. 
-"""
-
-
 quality_check_task_description = """
-    Read and analyze the tailored resume, compare it to the tailored resume.
-    Find embelishment, Check for redundant fields,
-    inserted skills and/or experience that are made up by
-    in the tailored resume and recommend correction.
-    Generate a revised tailored resume based on your findings.
-    Make sure that the revised tailored resume is honest and has integrity.
-    For example if the tailored resume inserted finance but it is not 
-    included in resume profile, then correct it.
+    These are the following quality checks to follow:
+    * Read and analyze the tailored resume, compare it to the tailored resume.
+    * Find embelishment, Check for redundant fields, inserted skills and/or experience that
+       made up by in the tailored resume and recommend correction.
+    * Generate a revised tailored resume based on the findings and recommendation.
+    * Make sure that the revised tailored resume is honest and has integrity. For example 
+      if the tailored resume inserted finance but it is not included in resume profile, 
+      then correct it.
     
 """
-
 quality_check_task_expected_output = """
-    revised tailored resume that is corrected by the quality checker. 
-    Detailed information on the changes after crafting the revised resume.
+    Revised tailored resume that is corrected by the quality check. Provide detailed
+    information on the changes after crafting the revised resume.
 """
 
+# Cover Letter Agent
+
+
+cover_letter_generator_goal = """
+    Generate a cover letter based that is also intentional rather than generic.
+"""
+cover_letter_generator_backstory = """
+    As a writer, you are capable of crafting a cover letter that respects inputs of 
+    the job applicant. You position the applicant in the best and honest way to the 
+    hiring manager.
+"""
 cover_letter_task_description = """
-    Using the profile, research and quality checked resume generated from
-    previous tasks, create a cover letter
-    using the following points outlined in ({cover_letter_inputs})
-    Only include skills and experience of the applicant relevant to the 
-    job description and do not exaggerate.
+    Using the profile, research and quality checked resume generated from previous 
+    tasks, create a cover letter using the following points outlined in cover letter 
+    inputs ({cover_letter_inputs}). Make a case to the hiring manager in an honest 
+    way. Do not embellish and exaggerate.
 """
-
 cover_letter_task_expected_output = """
     A one paragraph cover letter
-  
 """
 
+
+# Interview Preparation Agent
+
+interview_preparer_agent_goal = """
+    Create interview questions and talking points based on the aligned resume and job 
+    description and requirements
+"""
+interview_preparer_agent_backstory = """
+    As a tutor, your role is crucial in anticipating the dynamics of interviews. With 
+    your ability to formulate key questions  and talking points, you prepare candidates
+    for success, ensuring they can confidently address all aspects of the job they are 
+    applying for.
+"""
 interview_preparation_task_description = """
-    Create a set of potential interview questions and talking
-    points based on the quality checked resume and job requirements.
-    Utilize tools to generate relevant questions and discussion
-    points. Make sure to use these question and talking points to
-    help the candiadte highlight the main points of the resume
-    and how it matches the job posting.
+    Create a set of potential interview questions and talking points based on the 
+    quality checked resume and job requirements. Utilize tools to generate relevant
+    questions and discussion points. Make sure to use these question and talking points to
+    help the candiadte highlight the main points of the resume and how it matches the 
+    job posting.
 """
-
 interview_preparation_task_expected_output = """
     A document containing key questions and talking points
     that the candidate should prepare for the initial interview.
